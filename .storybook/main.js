3b1194b068930f5f7bf168946cb32c670cd5fd86
GuillaumeRZ <gui.remyzephir@gmail.com>
2020-04-12 18:43:32 +0200

    feat(global): reset project and add circleCI

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,10 @@
 module.exports = {
-  stories: ['../src/components/**/*.stories.js'],
-  addons: ['@storybook/preset-create-react-app', '@storybook/addon-actions', '@storybook/addon-knobs', '@storybook/addon-links'],
+  stories: ['../src/**/*.stories.js'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-storysource',
+    '@storybook/addon-knobs',
+  ],
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,10 @@
-import '../src/index.css';
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import { GlobalStyle } from '../src/shared/global';
+
+addDecorator(story => (
+  <>
+    <GlobalStyle />
+    {story()}
+  </>
+));

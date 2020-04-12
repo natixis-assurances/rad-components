f10321165a5d65c84fd06f189adb80b2a401553e
GuillaumeRZ <gui.remyzephir@gmail.com>
2020-04-12 19:30:08 +0200

    feat(global): add a11y addon and update test with circleCI

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,12 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  // automatically import all files ending in *.stories.js|mdx
+  stories: ['../src/**/*.stories.(js|mdx)'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-storysource',
     '@storybook/addon-knobs',
+    '@storybook/addon-a11y',
   ],
 };

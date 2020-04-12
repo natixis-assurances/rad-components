b6422d6d7354e21e3f193a45f0141154183f1b70
GuillaumeRZ <gui.remyzephir@gmail.com>
2020-04-12 10:13:24 +0200

    feat(task): Add knobs addons and handle long title

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../src/components/**/*.stories.js'],
-  addons: ['@storybook/preset-create-react-app', '@storybook/addon-actions', '@storybook/addon-links'],
+  addons: ['@storybook/preset-create-react-app', '@storybook/addon-actions', '@storybook/addon-knobs', '@storybook/addon-links'],
 };
